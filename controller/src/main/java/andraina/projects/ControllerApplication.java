7652cc0fe55f20447967239fc8bd1198bd19b58c
Gluon <andrainat@gmail.com>
2024-01-14 21:50:17 +0100

    Configuration inMemory storage

--- controller/src/main/java/andraina/projects/ControllerApplication.java
+++ controller/src/main/java/andraina/projects/ControllerApplication.java
@@ -2,8 +2,10 @@ package andraina.projects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ReposirotyConfig.class)
 public class ControllerApplication {
 
 	public static void main(String[] args) {
---
+++ repository/src/main/java/andraina/projects/ReposirotyConfig.java
@@ -0,0 +1,10 @@
+package andraina.projects;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:repository.properties")
+public class ReposirotyConfig {
+
+}
